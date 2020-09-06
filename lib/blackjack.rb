6764484ab1fdb89b23879797f224658e12cef5db
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-09-06 20:43:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,14 +60,14 @@ end
 def runner
   welcome
   initial_round
-  hit?(total)
-  user_input = get_user_input
-    if user_input == 's'
-      hit?(total)
-    elsif user_input == 'h'
-      deal_card
-    end
-    if total > 21
-    end_game(total)
-  end
+  # hit?(total)
+  # user_input = get_user_input
+  #   if user_input == 's'
+  #     hit?(total)
+  #   elsif user_input == 'h'
+  #     deal_card
+  #   end
+  #   if total > 21
+  #   end_game(total)
+  # end
 end
